013b7e5a5ea0740e67045202555b8f0ccde9af42
James Kyburz <james.kyburz@gmail.com>
2016-02-24 20:34:20 +0100

    opt.overlay

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function Gauge (opt) {
   this.state = {
     overlay: {
       style: {
+        display: typeof opt.overlay !== 'undefined' && !opt.overlay ? 'none' : '',
         position: 'fixed',
         width: '100%',
         height: '100%',
